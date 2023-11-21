429e146544fba3a04fabe29babbaaed3b051025c
AlexFlorenco <aflorenco13@gmail.com>
2023-11-20 21:25:21 -0300

    feat: Declaração de constantes de cores

---
+++ desafio_2/escriboteca/lib/app/constants.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+const primaryColor = Color(0xff369FFF);
+const secondaryColor = Color(0xff94D64B);
+const whiteText = Color(0xFFFFFFFF);
+
+const favoriteActive = Color(0xFFE53935);
+const favoriteInactive = Color(0x88FFFFFF);
+const favoriteBackground = Color(0x42000000);
