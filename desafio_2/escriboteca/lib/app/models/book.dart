70b62f077d0d7c1a0f2414a3afc13e2baca55840
AlexFlorenco <aflorenco13@gmail.com>
2023-11-21 23:41:22 -0300

    feat: Variáveis observáveis e funções de persistência de estado

--- desafio_2/escriboteca/lib/app/models/book.dart
+++ desafio_2/escriboteca/lib/app/models/book.dart
@@ -1,9 +1,14 @@
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class Book {
   final int id;
   final String title;
   final String author;
   final String coverUrl;
   final String downloadUrl;
+  RxBool isFavorite = false.obs;
+  RxBool isDownloaded = false.obs;
 
   Book({
     required this.id,
@@ -21,5 +26,25 @@ class Book {
       coverUrl: json['cover_url'],
       downloadUrl: json['download_url'],
     );
+  }
+
+  Future<void> saveDownloadedState() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDownloaded$id', isDownloaded.value);
+  }
+
+  Future<void> loadDownloadedState() async {
+    final prefs = await SharedPreferences.getInstance();
+    isDownloaded.value = prefs.getBool('isDownloaded$id') ?? false;
+  }
+
+  Future<void> saveFavoriteState() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isFavorite$id', isFavorite.value);
+  }
+
+  Future<void> loadFavoriteState() async {
+    final prefs = await SharedPreferences.getInstance();
+    isFavorite.value = prefs.getBool('isFavorite$id') ?? false;
   }
 }
