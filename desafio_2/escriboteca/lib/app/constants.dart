8286dc7e100df419b54d4159dbebe2a839fb7ebf
AlexFlorenco <aflorenco13@gmail.com>
2023-11-21 23:39:51 -0300

    style: Alteração e inclusão de cor

--- desafio_2/escriboteca/lib/app/constants.dart
+++ desafio_2/escriboteca/lib/app/constants.dart
@@ -6,4 +6,6 @@ const whiteText = Color(0xFFFFFFFF);
 
 const favoriteActive = Color(0xFFE53935);
 const favoriteInactive = Color(0x88FFFFFF);
-const favoriteBackground = Color(0x42000000);
+const favoriteBackground = Color(0x30000000);
+
+const downloadItemsColor = Color(0x73000000);
