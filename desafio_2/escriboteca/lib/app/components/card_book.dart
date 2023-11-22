c5fb9a23c7e0c6d4b4150d8c87f3c2938d627a42
AlexFlorenco <aflorenco13@gmail.com>
2023-11-21 23:37:43 -0300

    feat: Inclusão da Controller e alterações de UI

--- desafio_2/escriboteca/lib/app/components/card_book.dart
+++ desafio_2/escriboteca/lib/app/components/card_book.dart
@@ -2,73 +2,99 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../constants.dart';
-import '../models/book.dart';
+import '../controllers/book_controller.dart';
 
-class CardBook extends StatefulWidget {
-  final Book book;
+class CardBook extends StatelessWidget {
+  final BookController controller;
 
-  const CardBook({super.key, required this.book});
-
-  @override
-  State<CardBook> createState() => _CardBookState();
-}
-
-class _CardBookState extends State<CardBook> {
-  RxBool isFavorite = false.obs;
+  const CardBook({super.key, required this.controller});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: AlignmentDirectional.topEnd,
-      children: [
-        Column(
+    return Obx(
+      () => GestureDetector(
+        onTap: () async => controller.downloadAndOpenBook(),
+        child: Stack(
+          alignment: AlignmentDirectional.topEnd,
           children: [
-            SizedBox(
-              height: 200,
-              width: 160,
-              child: Image.network(
-                widget.book.coverUrl,
-                fit: BoxFit.fill,
+            Column(
+              children: [
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    SizedBox(
+                      height: 200,
+                      width: 160,
+                      child: Opacity(
+                        opacity: controller.book.isDownloaded.value ? 1.0 : 0.5,
+                        child: Image.network(
+                          controller.book.coverUrl,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    if (!controller.book.isDownloaded.value &&
+                        !controller.isDownloading.value)
+                      const Positioned.fill(
+                        child: Icon(
+                          Icons.file_download,
+                          size: 50,
+                          color: downloadItemsColor,
+                        ),
+                      ),
+                    if (controller.isDownloading.value)
+                      const Positioned.fill(
+                        child: Center(
+                          child: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: CircularProgressIndicator(
+                              strokeWidth: 6,
+                              color: downloadItemsColor,
+                            ),
+                          ),
+                        ),
+                      )
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  controller.book.title,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  controller.book.author,
+                  style: Theme.of(context).textTheme.bodySmall,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+            Obx(
+              () => GestureDetector(
+                onTap: () => controller.toggleFavorite(),
+                child: controller.book.isFavorite.value
+                    ? const CircleAvatar(
+                        backgroundColor: favoriteBackground,
+                        child: Icon(
+                          Icons.bookmark,
+                          color: favoriteActive,
+                          size: 30,
+                        ),
+                      )
+                    : const CircleAvatar(
+                        backgroundColor: favoriteBackground,
+                        child: Icon(
+                          Icons.bookmark_outline,
+                          color: favoriteInactive,
+                          size: 30,
+                        ),
+                      ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              widget.book.title,
-              style: Theme.of(context).textTheme.bodyMedium,
-              overflow: TextOverflow.ellipsis,
-            ),
-            Text(
-              widget.book.author,
-              style: Theme.of(context).textTheme.bodySmall,
-              overflow: TextOverflow.ellipsis,
             ),
           ],
         ),
-        Obx(
-          () => GestureDetector(
-            onTap: () {
-              isFavorite.value = !isFavorite.value;
-            },
-            child: isFavorite.value
-                ? const CircleAvatar(
-                    backgroundColor: favoriteBackground,
-                    child: Icon(
-                      Icons.bookmark,
-                      color: favoriteActive,
-                      size: 30,
-                    ),
-                  )
-                : const CircleAvatar(
-                    backgroundColor: favoriteBackground,
-                    child: Icon(
-                      Icons.bookmark_outline,
-                      color: favoriteInactive,
-                      size: 30,
-                    ),
-                  ),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
