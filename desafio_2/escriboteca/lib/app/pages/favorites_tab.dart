b7ec9016e25f30957a05cb20b96e583fdbdf1d69
AlexFlorenco <aflorenco13@gmail.com>
2023-11-22 21:55:21 -0300

    feat: Implementação do empty state e correção de bug no modo paisagem

--- desafio_2/escriboteca/lib/app/pages/favorites_tab.dart
+++ desafio_2/escriboteca/lib/app/pages/favorites_tab.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:escribopedia/app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -13,27 +16,49 @@ class FavoriteTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Obx(
-      () => GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisSpacing: 16,
-          crossAxisCount: 2,
-          childAspectRatio: 0.6,
-        ),
-        itemCount:
-            booksRepository.books.where((book) => book.isFavorite.value).length,
-        itemBuilder: (context, index) {
-          Book book = booksRepository.books
+      () => booksRepository.books
               .where((book) => book.isFavorite.value)
-              .toList()[index];
-          String controllerName = 'bookController${book.id}';
-          Get.put(BookController(book), tag: controllerName);
-          return Center(
-            child: CardBook(
-              controller: Get.find<BookController>(tag: controllerName),
+              .toList()
+              .isEmpty
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'lib/assets/images/empty_state.png',
+                  opacity: const AlwaysStoppedAnimation(0.6),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Nenhum resultado encontrado.\nExperimente favoritar um livro.',
+                  style: TextStyle(color: Colors.black54),
+                )
+              ],
+            )
+          : GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 16,
+                crossAxisCount:
+                    MediaQuery.of(context).orientation == Orientation.portrait
+                        ? 2
+                        : 4,
+                childAspectRatio: 0.6,
+              ),
+              itemCount: booksRepository.books
+                  .where((book) => book.isFavorite.value)
+                  .length,
+              itemBuilder: (context, index) {
+                Book book = booksRepository.books
+                    .where((book) => book.isFavorite.value)
+                    .toList()[index];
+                String controllerName = 'bookController${book.id}';
+                Get.put(BookController(book), tag: controllerName);
+                return Center(
+                  child: CardBook(
+                    controller: Get.find<BookController>(tag: controllerName),
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
